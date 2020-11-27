f5e3ed333004fc52b482bc60317ec7b6e1bbdd71
Gonzalo Moreira <gonzalo-adrian.moreira@atos.net>
2020-11-27 00:44:36 -0300

    se presenta un stepper

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CodeEditorComponent } from './code-editor/code-editor.component';
+import { StepFormComponent } from './step-form/step-form.component';
 
 
 const routes: Routes = [
   {path: 'code-editor', component: CodeEditorComponent},
+  {path: 'step-form', component: StepFormComponent},
   {path: '', redirectTo: '/', pathMatch: 'full'},
   // {path: '**', component: PageNotFoundComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { CodeEditorComponent } from './code-editor/code-editor.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { StepFormComponent } from './step-form/step-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CodeEditorComponent
+    CodeEditorComponent,
+    StepFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
