d9c0621d8fe06163b1559c984e4bc9a17edd080a
Gonzalo Adrián Moreira <gonzalo.moreira@live.com.ar>
2020-12-04 14:36:33 -0300

    avance home refactor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+const MODULES = [
+  { module: "exams", name: "Exams" },
+  { module: "candidates", name: "Candidates" },
+  { module: "reports", name: "Reports" },
+  { module: "admin", name: "Administration" },
+  { module: "security", name: "Security" },
+]
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-  
+  searchText
+
   constructor() {}
 
   ngOnInit() {}
